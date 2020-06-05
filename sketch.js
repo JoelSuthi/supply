909bcfd553a68d1f41918448f7810d1345bdf632
JoelSuthi <65766735+JoelSuthi@users.noreply.github.com>
2020-06-05 13:56:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,64 +1,55 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var ground,box,box2,box3,paper;
+var option = {
+isStatic:false,
+restitution:0.3,
+friction:0.5,
+density:1.2,	
+}
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-const Render = Matter.Render;
-var packageBody,ground,box1,box2,box3;
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png");
-	packageIMG=loadImage("package.png");
-	
-}
 
 function setup() {
 	createCanvas(800, 700);
-	//rectMode(CENTER);
-	packageSprite=createSprite(350,150, 10,10);
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6;
-
-	
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
 
 	engine = Engine.create();
 	world = engine.world;
-	
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
-	World.add(world, packageBody);
 
-	//Create a Ground
+	paper = Bodies.circle(200,100,20,option);
+    World.add(world,paper);
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255);
+
+	box=createSprite(500,610, 20,100);
+	box.shapeColor = "yellow";
+	box2 = createSprite(600,650,200,20);
+	box2.shapeColor = "yellow";
+	box3=createSprite(700,610,20,100);
+	box3.shapeColor = "yellow";
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	 World.add(world, ground);
-	 
+ 	World.add(world, ground);
+
+
 	Engine.run(engine);
   
 }
 
 
 function draw() {
-  //rectMode(CENTER);
+  rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  keyPressed();
+  ellipse(50,630,50,50);
   drawSprites();
- box1.display();
- box2.display();
- box3.display();
+  keyPressed();
+}
+function keyPressed(){
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper.Body,paper.body.position,{x:85,y:-90})
+
+	}
 }
 
-function keyPressed() {
-	if (keyCode === DOWN_ARROW) {
-	   Matter.Body.setStatic(packageBody,false);
-	   
-	 }
-   }
 
 
